e9d9f47b8eafbcea012128d2612c68bf5cee1a8e
balajiv <balajivenkat477@yahoo.com>
2019-04-18 11:32:28 +0530

    ASSIGNMENT_7

---
+++ assignment_7/js/script.js
@@ -0,0 +1,31 @@
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+
+function check() {
+    if (document.getElementById('business').checked) {
+        document.getElementById('businessData').style.display = 'flex';
+        document.getElementById('communityData').style.display = 'none';
+        document.getElementById('switching').style.left = "2px";
+
+    }
+    else {
+        document.getElementById('communityData').style.display = 'flex';
+        document.getElementById('businessData').style.display = 'none';
+        document.getElementById('switching').style.left = "90px";
+
+    }
+}
+
