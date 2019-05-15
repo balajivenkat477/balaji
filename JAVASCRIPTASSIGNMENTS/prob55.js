c1742e8cbcb0c230a9fbc1f653aae660687f830f
balajivenkat477 <balajivenkat477@yahoo.com>
2019-05-15 17:30:38 +0530

    NEW_CHANGES

--- JAVASCRIPTASSIGNMENTS/prob55.js
+++ JAVASCRIPTASSIGNMENTS/prob55.js
@@ -1,1 +1,20 @@
-    
+function spy(func) {
+    function wrapper(...args) {
+      wrapper.calls.push(args);
+      return func.apply(this, arguments);
+    }
+  
+    wrapper.calls = [];
+  
+    return wrapper;
+  }
+  
+  function work(a, b) {
+    alert( a + b ); 
+  }
+  work = spy(work);
+  work(1, 2); 
+  work(4, 5); 
+  for (let args of work.calls) {
+    alert( 'call:' + args.join() ); 
+  }
